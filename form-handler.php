31c10b16bf4d2f5c0c0415dc07e8f4322424fed6
Chandana1399 <84435299+Chandana1399@users.noreply.github.com>
2021-05-19 15:06:32 +0530

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'info@yourwebsite.com';
+$email_from = 'chandanareddyhp@gmail.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name:$name.\n".
               "User Email:$visitor_email.\n".
